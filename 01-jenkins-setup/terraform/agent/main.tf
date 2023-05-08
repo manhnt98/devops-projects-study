a86c0b6473f347f25caa5084d1dda2a6c9596571
manh.nguyentien@vndirect.com.vn <manh.nguyentien@vndirect.com.vn>
2023-05-08 16:09:18 +0700

    Update jenkins agent

--- 01-jenkins-setup/terraform/agent/main.tf
+++ 01-jenkins-setup/terraform/agent/main.tf
@@ -7,9 +7,9 @@ module "ec2_instance" {
   source = "../modules/ec2"
 
   instance_name      = "jenkins-agent"
-  ami_id             = "ami-0e68ab34763bcba1f"
+  ami_id             = "ami-05e5c2352b2f32bcb"
   instance_type      = "t2.small"
-  key_name           = "techiescamp"
+  key_name           = "Jenkins_HA"
   subnet_ids         = ["subnet-0755c5b9d5ced7842", "subnet-07ea2dc8a0c7a34d3", "subnet-0d916b3b2a20a0e89"]
   instance_count     = 1
 }

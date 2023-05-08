93ee4355e1b74e00bc479515487f856152c12a53
manh.nguyentien@vndirect.com.vn <contact.tienmanh@gmail.com>
2023-05-08 14:41:48 +0700

    Update jenkins controller tf

--- 01-jenkins-setup/terraform/lb-asg/main.tf
+++ 01-jenkins-setup/terraform/lb-asg/main.tf
@@ -6,9 +6,9 @@ provider "aws" {
 module "lb-asg" {
   source        = "../modules/lb-asg"
   subnets       = ["subnet-0755c5b9d5ced7842", "subnet-07ea2dc8a0c7a34d3", "subnet-0d916b3b2a20a0e89"]
-  ami_id        = "ami-074d40b56472c5b9b"
+  ami_id        = "ami-016ecf8b701f06a05"
   instance_type = "t2.small"
-  key_name      = "techiescamp"
+  key_name      = "Jenkins_HA"
   environment   = "dev"
-  vpc_id        = "vpc-0a5ca4a92c2e10163"
+  vpc_id        = "vpc-09e5e5d35e75df7bc"
 }
